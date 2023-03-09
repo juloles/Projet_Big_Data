cc9272049fa439fb5a122aa29394672a900a845f
juloles <79644337+juloles@users.noreply.github.com>
2023-03-09 15:11:29 +0100

    Test départ

---
+++ Declaration.java
@@ -0,0 +1,58 @@
+import java.util.Date;
+
+public class Declaration {
+    private int id;
+    private Date date;
+    private String typeAnomalie;
+    private int arrondissement;
+    private String outil;
+
+    public Declaration(int id, Date date, String typeAnomalie, int arrondissement, String outil) {
+        this.id = id;
+        this.date = date;
+        this.typeAnomalie = typeAnomalie;
+        this.arrondissement = arrondissement;
+        this.outil = outil;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTypeAnomalie() {
+        return typeAnomalie;
+    }
+
+    public void setTypeAnomalie(String typeAnomalie) {
+        this.typeAnomalie = typeAnomalie;
+    }
+
+    public int getArrondissement() {
+        return arrondissement;
+    }
+
+    public void setArrondissement(int arrondissement) {
+        this.arrondissement = arrondissement;
+    }
+
+    public String getOutil() {
+        return outil;
+    }
+
+    public void setOutil(String outil) {
+        this.outil = outil;
+    }
+}
+
